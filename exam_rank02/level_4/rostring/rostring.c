dfd2371fc7660789d99454d23dfb9b0c0ce92fa7
Youness Hachimi <yhachimi@c2r4p6.1337.ma>
2025-12-29 09:25:44 +0100

    comit after finish the  restring.c

--- exam_rank02/level_4/rostring/rostring.c
+++ exam_rank02/level_4/rostring/rostring.c
@@ -1,11 +1,40 @@
 #include "unistd.h"
-#include "stdlib.h"
-static int count()
-{
-}
 
 int main(int ac,char **av)
 {
 	if (ac < 2)
-		return (write(1,'\n',1),1);
+		return (write(1,"\n",1),1);
+	int	i = 0;
+	int check = 0;
+	while (av[1][i]  && av[1][i] == ' ')
+		i++;
+	while (av[1][i] && av[1][i] != ' ')
+		i++;
+	if (av[1][i])
+		++i;
+	while (av[1][i])
+	{
+		while (av[1][i] && av[1][i] != ' ')
+		{
+			write(1,&av[1][i],1);
+			check = 1;
+			i++;
+		}
+		while ( av[1][i] && av[1][i] == ' ')
+                	i++;
+		if (av[1][i])
+			write(1," ",1);
+	}
+	i = 0;
+	while ( av[1][i] && av[1][i] == ' ')
+                i++;
+	if (check == 1)
+		write(1," ",1);
+	while (av[1][i] && av[1][i] != ' ')
+	{
+		write(1,&av[1][i],1);
+		i++;
+	}
+	write(1,"\n",1);
+
 }
